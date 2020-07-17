c67bea270737767efcdd360103068ded9708b061
Stephen <clarksj4@student.op.ac.nz>
2020-07-18 11:11:31 +1200

    Add perpetual bopping mechanic

---
+++ Assets/Scripts/Bop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bop : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Raise()
+    {
+        animator.SetTrigger("Raise");
+    }
+
+    public void Depress()
+    {
+        animator.SetTrigger("Depress");
+    }
+}
