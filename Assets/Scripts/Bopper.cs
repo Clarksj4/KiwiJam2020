79de8abcb3798e62902317f8a9e8bddfb75b54a0
Stephen <clarksj4@student.op.ac.nz>
2020-07-17 21:47:37 +1200

    Add bopper script that can rotate a thing - add a cube on screen that implements it

---
+++ Assets/Scripts/Bopper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bopper : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 2f;
+
+    private Vector2 GetMouseInput()
+    {
+        return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+
+    private Vector2 GetKeyboardInput()
+    {
+        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = GetKeyboardInput();
+        Vector2 rotation = input * sensitivity * Time.deltaTime;
+
+        transform.Rotate(rotation.y, -rotation.x, 0, Space.World);
+    }
+}
